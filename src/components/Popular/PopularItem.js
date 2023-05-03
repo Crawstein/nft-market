3c5a6dcc2cab9a76e39a6357b5c47538da678edf
Crawstein <michael.shu@mintymint.net>
2023-05-04 00:36:10 +0300

    img path hot-fix

--- src/components/Popular/PopularItem.js
+++ src/components/Popular/PopularItem.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 function PopularItem(props) {
     return (
         <Link to={`/${props.name.toLowerCase().split(' ').join('-')}`} className="popular__content-item" >
-            <div className="popular__content-item-animation-helper" style={{ backgroundImage: `url(http://localhost:3000/static/media/arts/${props.name.toLowerCase().split(' ').join('-')}.jpg)` }}>
-                <div className="popular__content-item-avatar" style={{ backgroundImage: `url(http://localhost:3000/static/media/avatars/${props.name.toLowerCase().split(' ').join('-')}.png)` }}></div>
+            <div className="popular__content-item-animation-helper" style={{ backgroundImage: `url(https://nft-market.crawstein.com/static/media/arts/${props.name.toLowerCase().split(' ').join('-')}.jpg)` }}>
+                <div className="popular__content-item-avatar" style={{ backgroundImage: `url(https://nft-market.crawstein.com/static/media/avatars/${props.name.toLowerCase().split(' ').join('-')}.png)` }}></div>
                 <div className="popular__content-item-info">
                     <div className="popular__content-item-label">Owner</div>
                     <div className="popular__content-item-name">{props.name}</div>
